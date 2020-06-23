e68c5d247c3901713c8589a364c609e57946b156
Ryan Sowden <Ryan@Susans-MacBook-Pro.local>
2020-06-23 21:26:53 +1000

    Changed return statement to 1d

--- api/app.py
+++ api/app.py
@@ -3,11 +3,14 @@ from getstock import stock
 
 app = Flask(__name__)
 
+#App route for the getstock function
 @app.route('/getstock', methods=['POST'])
 def getStock():
-    result = stock().get_stock()
-    return result
+    result = stock().get_stock() #Setting results to the getstock function
+    return result #Returning the result
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+
--- api/getstock.py
+++ api/getstock.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request,abort
 import yfinance as yf
 import json
 import pandas as pd
@@ -10,14 +10,25 @@ class stock:
         self.info = ''
         self.df = ''
         self.choice = ''
+        self.result = ''
 
     def get_stock(self):
-        self.choice = request.form['ticker']
-        self.ticker = yf.Ticker(self.choice)
-        self.info = self.ticker.history(period='1d')
-        self.df = self.info
+        self.choice = request.form['ticker'] #Requesting stock ticker
+        self.ticker = yf.Ticker(self.choice) #Passing request through yf
+        self.info = self.ticker.history(period='1d') #Setting history of stock, can be changed.
+        self.df = self.info[['Close']] #setting results of request to DataFrame
+        self.df.drop(self.df.columns[[1]], axis=1)
         self.df.reset_index(inplace=True)
-        return(json.loads(self.df.to_json(orient='index')))
+        self.result = json.loads(self.df.to_json(orient='index'))
+
+        if self.df.empty: #Checking if DataFrame is empty, if empty pass 404 error
+            return abort(404, description="Ticker doesn't exist, please try again")
+        else:#Else return DataFrame
+            return self.result
+
+
+
+
         
 
 
